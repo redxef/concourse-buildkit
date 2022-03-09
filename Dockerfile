248cb76ac6f4d4caec0d57fc7cfaefdffabe98b5
redxef <federanko.manuel@gmail.com>
2022-03-09 14:19:24 +0100

    Add docker login.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM moby/buildkit
 
+COPY entrypoint.sh /usr/local/bin/entrypoint.sh
 ENTRYPOINT [ "entrypoint.sh" ]
 
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env sh
 
-buildctl-deamonless.sh
+DOCKER_LOGIN_FILE_TMPL='{
+    "auths": {
+        "{{REGISTRY_URL}}": {
+            "auth": "{{BASE64_UNAME_PW}}"
+        }
+    }
+}'
 
-"$@"
+docker_login() {
+    # TODO: detect registry url
+    mkdir -p "$HOME/.docker"
+    echo "$DOCKER_LOGIN_FILE_TMPL" | \
+        sed -e "s|{{BASE64_UNAME_PW}}|$(printf '%s:%s' "$username" "$password" | base64)|g" \
+            -e "s|{{REGISTRY_URL}}|https://index.docker.io/v1/|g" \
+        > "$HOME/.docker/config.json"
+}
+
+if [ -n "$username" ]; then
+    docker_login
+fi
+
+buildctl-daemonless.sh "$@"
